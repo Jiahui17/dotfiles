28adb56c929de67b8ba12b24af640173f3bad87c
Jiahui Xu <jxu@ethz.ch>
2023-10-19 13:08:37 +0200

    update script for setup user python environment

---
+++ .local/bin/deb-setup-py.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# This is a script I use to create a virtual environment for user packages
+
+VENV_DIR=~/.local/pyenv
+
+pyvenv () {
+	python3 -m venv $VENV_DIR
+
+	# add dot2smv project to the PYTHONPATH
+	source $VENV_DIR/bin/activate
+
+	# Make sure that the pip version is up-to-date
+	python3 -m pip install --upgrade pip
+}
+
+[ -f "$VENV_DIR/bin/activate" ] || pyvenv
